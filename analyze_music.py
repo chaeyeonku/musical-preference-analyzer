fc4692578115dd73a1a22f46bedff72e469a8417
chaeyeonku <applekoo63@gmail.com>
2022-12-04 20:52:38 -0800

    Add waveform plot and chord recognition plot

--- analyze_music.py
+++ analyze_music.py
@@ -5,61 +5,42 @@ import matplotlib.pyplot as plt
 import libfmp.c5
 import libfmp.b
 
-# returns chromagram and chord annotation results
-def analyze_chromagram(y: np.ndarray, sr: int):
-    chromagram = librosa.feature.chroma_cqt(y=y, sr=sr)
+# returns chromagram and chord recognition results
+def analyze_chromagram(X_CQT, Fs_X, x, Fs, x_dur):
 
-    # TODO get chord annotation using libfmp library
     # chord recognition
-    wav_filename = "./sample/pretend-to-be.wav"
-    X_CQT, Fs_X, x, Fs, x_dur = libfmp.c5.compute_chromagram_from_filename(wav_filename, N=4096, H=2048, version="CQT")
     chord_sim, chord_max = libfmp.c5.chord_recognition_template(X_CQT, norm_sim="max")
     chord_labels = libfmp.c5.get_chord_labels()
 
-    # print(chord_sim, chord_max)
-    print(chord_max)
-    print(chord_labels)
+    # figure settings
+    fig, ax = plt.subplots(3, 2, gridspec_kw={'width_ratios': [1, 0.03], 
+                                            'height_ratios': [1, 1, 2]}, figsize=(9, 6))
 
-    #Plot
-    fig, ax = plt.subplots(2, 2, gridspec_kw={'width_ratios': [1, 0.03], 
-                                            'height_ratios': [1, 2]}, figsize=(9, 6))
+    # plot waveform
+    libfmp.b.plot_signal(x, Fs, ax=ax[0,0], title='Waveform of audio signal')
 
+    # plot chromagram
     title = 'Chromagram (N = %d)' % X_CQT.shape[1]
-    libfmp.b.plot_chromagram(X_CQT, Fs=1, ax=[ax[0, 0], ax[0, 1]],
+    libfmp.b.plot_chromagram(X_CQT, Fs=1, ax=[ax[1, 0], ax[1, 1]],
                             chroma_yticks = [0, 4, 7, 11], clim=[0, 1], cmap='gray_r',
                             title=title, ylabel='Chroma', colorbar=True)
 
+    # plot chord recognition
     title = 'Time–chord representation of chord recognition result (N = %d)' % X_CQT.shape[1]
-    libfmp.b.plot_matrix(chord_max, ax=[ax[1, 0], ax[1, 1]], Fs=1, 
+    libfmp.b.plot_matrix(chord_max, ax=[ax[2, 0], ax[2, 1]], Fs=1, 
                         title=title, ylabel='Chord', xlabel='Time (frames)')
-    ax[1, 0].set_yticks(np.arange(len(chord_labels)))
-    ax[1, 0].set_yticklabels(chord_labels)
-    ax[1, 0].grid()
+    ax[2, 0].set_yticks(np.arange(len(chord_labels)))
+    ax[2, 0].set_yticklabels(chord_labels)
+    ax[2, 0].grid()
+
     plt.tight_layout()
-
-    return chromagram
-
-def run_feature_extraction(filename: str):
-    # loads an audio file as float ndarray
-    y, sr = librosa.load(filename, duration=12)
-
-    # separate harmonics and percussives
-    y_harmonic, y_percussive = librosa.effects.hpss(y)
-
-    tempo, _ = librosa.beat.beat_track(y=y_percussive, sr=sr)
-
-    # compute chromagram
-    chromagram = analyze_chromagram(y=y_harmonic, sr=sr)
-
-    # plot pitch information 
-    fig, ax = plt.subplots(nrows=2, sharex=True, sharey=True)
-    img = librosa.display.specshow(chromagram, y_axis='chroma', x_axis='time', ax=ax[0])
-    ax[0].set(title='chroma')
-    ax[0].label_outer()
-
-    fig.colorbar(img, ax=ax)
-
     plt.show()
 
+def run_feature_extraction(filename: str):
+    # load audio from file
+    X_CQT, Fs_X, x, Fs, x_dur = libfmp.c5.compute_chromagram_from_filename(filename, N=4096, H=2048, version="CQT")
+
+    analyze_chromagram(X_CQT, Fs_X, x, Fs, x_dur)
+
 # testing
-run_feature_extraction("./sample/pretend-to-be.mp3")
+run_feature_extraction("./sample/pretend-to-be.wav")
